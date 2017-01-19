63705f10bdae8aff14a6f74c7f15f9deeedddb2b
emulatrix <grant.bentley@gmail.com>
2017-01-19 18:04:31 +0000

    test commit
    no actual change made, just a comment to alter the file

--- temp_logger_v3.py
+++ temp_logger_v3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # import python modules required
-import suds 
+import suds
 import os
 import glob
 import time
@@ -17,13 +17,13 @@ os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28-0000053e5148')[0]
 device_file = device_folder + '/w1_slave'
- 
+
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
- 
+
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -33,14 +33,14 @@ def read_temp():
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.00
-    return temp_c    
- 
+    return temp_c
+
 # Variables for Web Service Values
 temp = (float(read_temp()))
 datetime = (time.strftime("%Y-%m-%d") +'T'+ time.strftime("%H:%M:00"))
 device_id = 'pi00000001'
- 
-#send to web Service
+
+#send to web Service on vm-hercules
 client = Client('http://vm-hercules:1121/WcfTempDemo.svc?WSDL')
 try:
     result = client.service.SaveTemperature(device_id,datetime,temp)
